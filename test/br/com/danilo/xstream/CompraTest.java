e3c23c7a91a43526098851c737537918a2861bc5
Danilo Righetto <danilo.righetto@fornax.com.br>
2016-06-01 22:30:47 -0300

    xstream - relacionamento entre objetos

--- test/br/com/danilo/xstream/CompraTest.java
+++ test/br/com/danilo/xstream/CompraTest.java
@@ -47,5 +47,44 @@ public class CompraTest {
 		
 		String xmlGerado = xstream.toXML(compra);
 		assertEquals(xmlEsperado, xmlGerado);
+		
+		/*Transformando XML em Objeto JAVA*/
+	}
+		
+		@Test
+		public void deveGerarUmaCompraComCadaUmDosProdutosDoXML(){
+			String xmlDeOrigem = "<compra>\n"+
+					"  <id>15</id>\n"+
+					"  <produtos>\n"+
+					"    <produto codigo=\"1587\">\n"+
+					"      <nome>geladeira</nome>\n"+
+					"      <preco>1000.0</preco>\n"+
+					"      <descrição>geladeira de duas portas</descrição>\n"+
+					"    </produto>\n"+
+					"    <produto codigo=\"1588\">\n"+
+					"      <nome>ferro de passar</nome>\n"+
+					"      <preco>100.0</preco>\n"+
+					"      <descrição>ferro com vaporizador</descrição>\n"+
+					"    </produto>\n"+
+					"  </produtos>\n"+
+				    "</compra>";
+			
+			Produto geladeira = new Produto("geladeira", 1000.0, "geladeira de duas portas", 1587);
+			Produto ferro = new Produto("ferro de passar", 100.0, "ferro com vaporizador", 1588);
+			
+			List<Produto> produtos = new ArrayList<>();
+			produtos.add(geladeira);
+			produtos.add(ferro);
+			Compra compraEsperada = new Compra(15, produtos);
+			
+			XStream xstream = new XStream();
+			xstream.alias("produto", Produto.class);
+			xstream.alias("compra", Compra.class);
+			xstream.aliasField("descrição", Produto.class, "descricao");
+			xstream.useAttributeFor(Produto.class, "codigo");
+			
+			Compra compraDeserializada = (Compra)xstream.fromXML(xmlDeOrigem);
+			
+			assertEquals(compraEsperada, compraDeserializada);
 	}
 }
